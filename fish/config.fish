d8fe133bfe145c5f93a166b3a0300de9ba72c024
jnst <jnst@outlook.com>
2017-04-18 18:52:45 +0900

    Update fish config for golang v1.8

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,6 @@ set -gx LSCOLORS gxfxcxdxbxegedabagacad
 # node.js
 set -gx PATH $HOME/.nodebrew/current/bin $PATH
 
-# golang
-set -gx GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
-
-alias history "history --show-time='%Y-%m-%d %T | '"
+alias history "history --show-time='%Y-%m-%d %T '"
 alias ll "ls -AlhF"
+
