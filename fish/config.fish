cb23fd06810db482a19f852e5c1b2183ef6dc1c1
jnst <jnst@outlook.com>
2020-12-24 01:57:39 +0900

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,18 @@ set -gx LSCOLORS gxfxcxdxbxegedabagacad
 # golang
 set -gx GOBIN $HOME/go/bin
 set -gx PATH $GOBIN $PATH
+set -gx GHQ_ROOT $HOME/go
+
+# goenv
+set -gx GOENV_ROOT $HOME/.goenv
+set -gx PATH $GOENV_ROOT/bin $PATH
+eval (goenv init - | source)
 
 alias history "history --show-time='%Y-%m-%d %T '"
 alias ll "ls -AlhF"
+alias k "kubectl"
+
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
